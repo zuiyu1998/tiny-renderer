6716d306bfa9aa7995f11c42eb7d1f501ba063a7
zuiyu1998 <1542844298@qq.com>
2024-10-16 11:27:11 +0800

    feat: 添加FrameGraph

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use std::{cell::UnsafeCell, rc::Rc};
+
+///FrameGraph是一个有向无环图，用于渲染数据的整合，cocos的rust版本
+pub struct FrameGraph {}
+
+impl FrameGraph {
+    ///compile阶段
+    pub fn compile(&mut self) {}
+
+    ///execute阶段
+    pub fn execute(&mut self) {}
+}
